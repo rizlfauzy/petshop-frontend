29c9bb3d6eaac8bedde28a6da07dc766a43ace21
Rizal Fauzi <rizalfauzi774@gmail.com>
2024-07-04 16:55:06 +0700

    [MINOR] update vite

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    open: true
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  base: "/"
 })

29a04b46b9a56d428e73167c91391f13fd44fd30
Rizal Fauzi <rizalfauzi774@gmail.com>
2024-08-15 19:09:08 +0700

    [REFACTOR] socket connection URL handling

--- src/utils/socket.jsx
+++ src/utils/socket.jsx
@@ -2,7 +2,10 @@ import { io } from "socket.io-client";
 
 const { VITE_BACKEND } = import.meta.env;
 
-const socket = io(VITE_BACKEND, {
+const arr_url = VITE_BACKEND.split("/");
+
+const socket = io(arr_url.splice(0, 3).join("/"), {
+  path: arr_url.join('/') == "" ? "/socket.io" : `/${arr_url.join('/')}/socket.io`,
   withCredentials: true,
 });
 
